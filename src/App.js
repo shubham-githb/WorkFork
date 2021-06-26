c5b7db1217c7759d42c229155e17d7b16e363a94
sk0999 <shubhamaother@gmail.com>
2021-06-27 00:16:15 +0530

    New changes version 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
 import "./App.css"
 import Header from "./Header";
 import Sidebar from "./Sidebar"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Chat from './Chat';
+import Login from './Login';
+import { useDebugValue } from "react"
 
 
 
 function App() {
+
+
+  const [user, setUser] = useState(null);
+
   return (
     <div className="app">
      <Router>
+     {!user ? (
+       <Login/>
+     ):(
+
+       <>
       <Header/>
       <div className="app__body">
         <Sidebar />
@@ -25,6 +36,9 @@ function App() {
 
         </Switch>
       </div>
+
+      </>
+     )}
      </Router>
     </div>
      
